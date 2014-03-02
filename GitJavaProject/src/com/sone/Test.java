0ef8c1503408f106838dd75030ae90783e136db6
sidhartha24 <sidhui.pani@gmail.com>
2014-03-02 20:32:41 +0530

    changes in 7

--- GitJavaProject/src/com/sone/Test.java
+++ GitJavaProject/src/com/sone/Test.java
@@ -42,7 +42,7 @@ public class Test {
        System.out.println("hellos");
        System.out.println("phellsos");
 
-       System.out.println("phellsosnew 66");
+       System.out.println("phellsosnew gg66");
 
 
 	}
