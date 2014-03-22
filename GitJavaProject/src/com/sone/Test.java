37278fe1d51200d7e7e10c3dea147e47c33b8072
sidhartha24 <sidhui.pani@gmail.com>
2014-03-22 14:02:44 +0530

    new ones

--- GitJavaProject/src/com/sone/Test.java
+++ GitJavaProject/src/com/sone/Test.java
@@ -42,7 +42,11 @@ public class Test {
        System.out.println("hellos");
        System.out.println("phellsos");
 
+
        System.out.println("psssheggggllsoffsnew gg6d6l");
+
+       System.out.println("phellsosnew gg6d6l");
+
 
 
 	}
